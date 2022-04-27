9a540925f7e44842ef9ec1974bed3cdc126ccd1c
OsmanYILMZ61 <trabzon_uzun_sokak@hotmail.com>
2022-04-28 00:21:11 +0300

    dublaj
    düzeltilecek kısımlar var

--- Assets/Scripts/Sequences/FirstTrigger.cs
+++ Assets/Scripts/Sequences/FirstTrigger.cs
@@ -21,7 +21,7 @@ public class FirstTrigger : MonoBehaviour
 	IEnumerator ScenePlayer() {
 		TextBox.GetComponent<Text> ().text = "Looks like a FLASHLIGHT on that table.";
 		line03.Play();
-		yield return new WaitForSeconds (2.5f);
+		yield return new WaitForSeconds (3.5f);
 		TextBox.GetComponent<Text> ().text = "";
 		ThePlayer.GetComponent<FirstPersonController> ().enabled = true;
 		TheMarker.SetActive (true);
